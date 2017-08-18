b3f7fd87a603024dfc5f7f10b90405c3fc7a6e2d
John-PC\John <johnmccormack1990@gmail.com>
2017-08-18 17:08:12 +0100

    42_MovementComponentAdded

--- BattleTank/Source/BattleTank/Private/TankTrack.cpp
+++ BattleTank/Source/BattleTank/Private/TankTrack.cpp
@@ -7,4 +7,8 @@ void UTankTrack::SetThrottle(float Speed)
 {
 	auto Name = GetOwner()->GetName();
 	UE_LOG(LogTemp, Warning, TEXT("%s throttling: %f"), *Name, Speed);
+	auto ForceApplied = GetForwardVector() * Speed * TrackMaxDrivingForce;
+	auto ForceLocation = GetComponentLocation();
+	auto TankRoot = Cast<UPrimitiveComponent>(GetOwner()->GetRootComponent());
+	TankRoot->AddForceAtLocation(ForceApplied, ForceLocation);
 }
--- BattleTank/Source/BattleTank/Public/TankTrack.h
+++ BattleTank/Source/BattleTank/Public/TankTrack.h
@@ -15,4 +15,7 @@ class BATTLETANK_API UTankTrack : public UStaticMeshComponent
 public:
 	UFUNCTION(BlueprintCallable, Category = Input)
 	void SetThrottle(float Throttle);
+
+	UPROPERTY(EditDefaultsOnly)
+	float TrackMaxDrivingForce = 400000;
 };
